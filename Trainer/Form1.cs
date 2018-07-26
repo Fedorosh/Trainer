10688bd0fefbaeace1e05b78578fe31f341b9283
Fedorosh <Vapaiss1@gmail.com>
2018-07-26 03:20:12 +0200

    naprawione odpalanie przed grą

--- Trainer/Form1.cs
+++ Trainer/Form1.cs
@@ -20,19 +20,29 @@ namespace Trainer
         }
 
         public Mem m = new Mem();
-
-        private void Form1_Load(object sender, EventArgs e)
+        public int ID = 0;
+        public bool czy = false;
+        public void Uruchom()
         {
-            int ID = m.getProcIDFromName("toy2.exe");
-            if (ID == 0) { MessageBox.Show("Nie ma procesu"); }
-            else m.OpenProcess("toy2.exe");
-            if (!backgroundWorker1.IsBusy) backgroundWorker1.RunWorkerAsync();
+            m.OpenProcess("toy2.exe");
             if (m.read2Byte("0x0052F396") != 14) m.writeMemory("0x0052F396", "int", "14"); //buzz's HP
             m.writeMemory("0x0040732A", "byte", "0x0"); //invincible buzz
             m.writeMemory("0x004E067C", "byte", "0xC"); //laser
             m.writeMemory("0x004E0676", "byte", "0x14"); //whirl
             m.writeMemory("0x004E068E", "byte", "0x14"); //stomp
             m.writeMemory("0x004E0688", "byte", "0x14"); //disks
+            czy = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ID = m.getProcIDFromName("toy2.exe");
+            if (!backgroundWorker1.IsBusy) backgroundWorker1.RunWorkerAsync();
+            if (ID == 0) { MessageBox.Show("Nie ma procesu"); }
+            else
+            {
+                Uruchom();
+            } 
 
         }
 
@@ -40,10 +50,15 @@ namespace Trainer
         {
             while(true)
             {
-                if (m.read2Byte("0x0052F39A") < 1 || m.read2Byte("0x0052F39A") > 1) m.writeMemory("0x0052F39A", "int", "1");
-                if (m.read2Byte("0x0052F39E") < 50) m.writeMemory("0x0052F39E", "int", "50");
-                if (m.read2Byte("0x0052B7D8") < 5) m.writeMemory("0x0052B7D8", "int", "5");
-                if (m.getProcIDFromName("toy2.exe") == 0) { MessageBox.Show("Nie ma procesu"); break; }
+                if(ID == 0) ID = m.getProcIDFromName("toy2.exe");
+                    if (ID != 0)
+                    {
+                    Uruchom();
+                        if (m.read2Byte("0x0052F39A") < 1 || m.read2Byte("0x0052F39A") > 1) m.writeMemory("0x0052F39A", "int", "1");
+                        if (m.read2Byte("0x0052F39E") < 50) m.writeMemory("0x0052F39E", "int", "50");
+                        if (m.read2Byte("0x0052B7D8") != 5) m.writeMemory("0x0052B7D8", "int", "5");
+                    //if (m.getProcIDFromName("toy2.exe") == 0) { MessageBox.Show("Nie ma procesu"); break; }
+                }
             }
 
         }
