35f11feedff67fc8944de21be832c3f20f357879
Fedorosh <Vapaiss1@gmail.com>
2018-08-27 17:52:17 +0200

    cofnięcie

--- Trainer/Properties/AssemblyInfo.cs
+++ Trainer/Properties/AssemblyInfo.cs
@@ -14,20 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-//Ustawienie wartości false dla elementu ComVisible sprawia, że typy w tym zestawie nie będą widoczne
-// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z 
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
 // COM, ustaw wartość true dla atrybutu ComVisible tego typu.
 [assembly: ComVisible(false)]
 
 // Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
-[assembly: Guid("ca8f6160-2ade-42b1-b84b-19fb0a950ff7")]
+[assembly: Guid("ca7bcc5a-bcd3-4315-a426-0341791e6bc4")]
 
 // Informacje o wersji zestawu zawierają następujące cztery wartości:
 //
 //      Wersja główna
-//      Wersja pomocnicza 
+//      Wersja pomocnicza
 //      Numer kompilacji
 //      Rewizja
 //
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
